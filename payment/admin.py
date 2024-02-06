affef9970c58369e0d9fb6976fa05a4f22d81749
KirillMelanich <kirillmelanich@gmail.com>
2024-02-06 14:05:03 +0200

    Register admin for payment app

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem, ShippingAddress
+
+admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
