3753bd7dbc10218106a86a0d658350c5ecd637c1
KirillMelanich <kirillmelanich@gmail.com>
2024-02-06 15:44:40 +0200

    Create views for payment app with html templates

--- payment/urls.py
+++ payment/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("checkout/", views.checkout, name="checkout"),
     path("complete-order/", views.complete_order, name="complete-order"),
     path("payment-success/", views.payment_success, name="payment-success"),
-    path("payment-fail/", views.payment_fail, name="payment-fail"),
+    path("payment-failed/", views.payment_fail, name="payment-failed"),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,26 +1,98 @@
-from django.shortcuts import render
+from .forms import ShippingAddressForm
+
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from cart.cart import Cart
+
+from .models import Order, OrderItem, ShippingAddress
 
 
+@login_required(login_url="account:login")
 def shipping(request):
-    # return render(request, "payment/shipping.html")
-    ...
+    try:
+        shipping_address = ShippingAddress.objects.get(user=request.user)
+    except ShippingAddress.DoesNotExist:
+        shipping_address = None
+
+    form = ShippingAddressForm(instance=shipping_address)
+
+    if request.method == "POST":
+        form = ShippingAddressForm(request.POST, instance=shipping_address)
+        if form.is_valid():
+            shipping_address = form.save(commit=False)
+            shipping_address.user = request.user
+            form.save()
+            return redirect("account:dashboard")
+
+    return render(request, "payment/shipping.html", {"form": form})
 
 
 def checkout(request):
-    # return render(request, "payment/checkout.html")
-    ...
+    if request.user.is_authenticated:
+        shipping_address = get_object_or_404(ShippingAddress, user=request.user)
+        if shipping_address:
+            return render(request, "payment/checkout.html", {"shipping_address": shipping_address})
+    return render(request, "payment/checkout.html")
 
 
 def complete_order(request):
-    # return render(request, "payment/complete-order.html")
-    ...
+    if request.POST.get("action") == "payment":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        street_address = request.POST.get("address1")
+        apartment_address = request.POST.get("address2")
+        city = request.POST.get("city")
+        country = request.POST.get("state")
+        zip_code = request.POST.get("zip_code")
+
+        cart = Cart(request)
+        total_price = cart.get_total_price()
+
+        shipping_address, _ = ShippingAddress.objects.get_or_create(
+            user=request.user,
+            defaults= {
+                "full_name": name,
+                "email": email,
+                "street_address": street_address,
+                "apartment_address": apartment_address,
+                "city": city,
+                "country": country,
+                "zip_code": zip_code
+            }
+        )
+
+        if request.user.is_authenticated:
+            order = Order.objects.create(user=request.user, shipping_address=shipping_address, amount=total_price)
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["qty"],
+                    user=request.user,
+                )
+        else:
+            order = Order.objects.create(shipping_address=shipping_address, amount=total_price)
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["qty"],
+                )
+
+        return JsonResponse({"success": True})
 
 
 def payment_success(request):
-    # return render(request, "payment/payment-success.html")
-    ...
+    for key in list(request.session.keys()):
+        del request.session[key]
+    return render(request, "payment/payment-success.html")
 
 
 def payment_fail(request):
-    # return render(request, "payment/payment-fail.html")
-    ...
+    return render(request, "payment/payment-failed.html")
